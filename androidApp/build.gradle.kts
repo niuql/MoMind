e4fd5cce5c4f0b589d8ed00ac9d0d12beb3fde98
Niu <niu@itmob.com>
2022-03-23 18:56:26 +0800

    Update libraries for sprint 1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.compose") version "1.0.0-beta5"
+    id("org.jetbrains.compose") version "1.1.1"
     id("com.android.application")
     kotlin("android")
 }
@@ -15,7 +15,7 @@ repositories {
 
 dependencies {
     implementation(project(":common"))
-    implementation("androidx.activity:activity-compose:1.3.0")
+    implementation("androidx.activity:activity-compose:1.4.0")
 }
 
 android {
